cd7f0d33c3216db2f55d7649d62953375a756eaf
qarq90 <scythethroughtheheart05@gmail.com>
2024-03-26 11:17:19 +0000

    Bug Fixes

--- components/quiz/CustomRadioBox.jsx
+++ components/quiz/CustomRadioBox.jsx
@@ -4,22 +4,24 @@ import {cn, Radio} from "@nextui-org/react";
 export const CustomRadioBox = (props) => {
     const {children, isInvalid, ...otherProps} = props;
     return (
-        <Radio
-            {...otherProps}
-            color="warning"
-            classNames={{
-                base: cn(
-                    "w-full bg-content1 m-0",
-                    "hover:bg-content2 items-center justify-start",
-                    "cursor-pointer rounded-lg gap-2 p-4 border-2 border-transparent",
-                    isInvalid
-                        ? "data-[selected=true]:border-danger"
-                        : "data-[selected=true]:border-warning"
-                ),
-                label: "w-full",
-            }}
-        >
-            {children}
-        </Radio>
+        <div className="w-full">
+            <Radio
+                {...otherProps}
+                color="warning"
+                classNames={{
+                    base: cn(
+                        "w-full bg-content1 m-0",
+                        "hover:bg-content2 items-center justify-start",
+                        "cursor-pointer rounded-lg gap-2 p-4 border-2 border-transparent",
+                        isInvalid
+                            ? "data-[selected=true]:border-danger"
+                            : "data-[selected=true]:border-warning"
+                    ),
+                    label: "w-full",
+                }}
+            >
+                {children}
+            </Radio>
+        </div>
     );
 };
--- components/quiz/QuestionCard.jsx
+++ components/quiz/QuestionCard.jsx
@@ -5,18 +5,18 @@ import {CustomRadioBox} from "@/components/quiz/CustomRadioBox";
 const QuestionCard = ({question, options, opt, onChange, idx}) => {
     return (
         <>
-            <div className="w-1/2 relative bg-neutral-800 rounded-lg p-4 items-center gap-4">
+            <div className="relative bg-neutral-800 rounded-lg p-4 items-center gap-4 h-auto lg:w-1/2 w-full">
                 <Card radius="sm">
                     <CardBody className="border-warning">
                         <p>{`${idx + 1}. ${question}`}</p>
                     </CardBody>
                 </Card>
                 <Spacer y="5"/>
-                <div className="w-full flex flex-col items-left gap-4 justify-around radio-btn">
+                <div className="max-w-full flex flex-col items-left gap-4 justify-around radio-btn">
                     <RadioGroup>
                         {options.map((option, idx) => (
                             <CustomRadioBox
-                                className="max-w-full rounded-lg"
+                                className="max-w-full"
                                 key={idx}
                                 value={opt[idx]}
                                 onChange={() => onChange(opt[idx], idx)}
